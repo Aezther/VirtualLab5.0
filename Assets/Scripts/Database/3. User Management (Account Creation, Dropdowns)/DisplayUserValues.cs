c4af765079388d3c42516704620e45d049c2de39
Set Olivarez <79820169+SetOlivrz@users.noreply.github.com>
2023-01-12 05:18:29 +0800

    Revised Display Student Data

--- Assets/Scripts/Database/3. User Management (Account Creation, Dropdowns)/DisplayUserValues.cs
+++ Assets/Scripts/Database/3. User Management (Account Creation, Dropdowns)/DisplayUserValues.cs
@@ -13,44 +13,57 @@ using System.Xml.Linq;
 public class DisplayUserValues : MonoBehaviour
 {
     [Header("TEXTS")]
-    public TextMeshProUGUI UserID;
-    public TextMeshProUGUI UserName;
-    public TextMeshProUGUI FirstName;
-    public TextMeshProUGUI MiddleName;
-    public TextMeshProUGUI LastName;
-    public TextMeshProUGUI Section;
+
+
+    [SerializeField] GameObject headerPrefab;
+    [SerializeField] GameObject contentParent;
+    private TextMeshProUGUI[] textCompList;
+
+
 
     private string connectionString;
     void Start()
     {
         connectionString = "URI=file:" + Application.streamingAssetsPath + "/Database/" + "/VirtualDB.db";
         DisplayToScrollView();
+
     }
 
-    public void DisplayToScrollView() {
+    public void DisplayToScrollView() 
+    {
+        // delete all child 
+        foreach (Transform child in contentParent.transform)
+        {
+            Destroy(child.gameObject);
+        }
 
-        UserID.text = "";
-        UserName.text = "";
-        FirstName.text = "";
-        MiddleName.text = "";
-        LastName.text = "";
-        Section.text = "";
-        using (IDbConnection dbConnection = new SqliteConnection(connectionString)) {
+        using (IDbConnection dbConnection = new SqliteConnection(connectionString)) 
+        {
             dbConnection.Open();
-            using (IDbCommand dbCmd = dbConnection.CreateCommand()) {
-                string sqlQuery = "SELECT * FROM TeachersTBL";
+            using (IDbCommand dbCmd = dbConnection.CreateCommand()) 
+            {
+                string sqlQuery = "SELECT StudentID, Username, Firstname, Middlename, Lastname, Section FROM StudentsTBL";
                 dbCmd.CommandText = sqlQuery;
-                using (IDataReader reader = dbCmd.ExecuteReader()) {
-                    while (reader.Read()) {
+                using (IDataReader reader = dbCmd.ExecuteReader()) 
+                {
+                    // reinstantiate all child
+                    while (reader.Read()) 
+                    {
+                        // one loop = 1 user
+                        Debug.Log(reader.GetInt32(0).ToString() + " " + reader.GetString(1) + " " + reader.GetString(2) + " " + reader.GetString(3) + " " + reader.GetString(4) + " " + reader.GetString(5));
+                        // create prefab
+                        // modify value
+                        GameObject userHeader = GameObject.Instantiate(headerPrefab, contentParent.transform);
 
-                        UserID.text += reader.GetInt32(0).ToString() + "\n";
-                        UserName.text += reader.GetString(1) + "\n";
-                        FirstName.text += reader.GetString(3) + "\n";
-                        MiddleName.text += reader.GetString(4) + "\n";
-                        LastName.text += reader.GetString(5) + "\n";
-                        Section.text += "Teacher" + "\n";
+                        textCompList = userHeader.gameObject.GetComponentsInChildren<TextMeshProUGUI>();
 
-
+                            Debug.Log(textCompList[0].gameObject.name);
+                            textCompList[0].text = reader.GetInt32(0).ToString();
+                            textCompList[1].text = reader.GetString(1);
+                            textCompList[2].text = reader.GetString(2);
+                            textCompList[3].text = reader.GetString(3);
+                            textCompList[4].text = reader.GetString(4);
+                            textCompList[5].text = reader.GetString(5);
                     }
                     reader.Close();
 
